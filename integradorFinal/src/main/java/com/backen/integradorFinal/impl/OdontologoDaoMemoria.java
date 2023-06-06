af54d974c174098a785c1d005aeba9e84866184f
pedroodelap <pedro.delap@hotmail.com>
2023-06-05 23:58:33 -0300

    Se edito la clase odontologo service

--- integradorFinal/src/main/java/com/backen/integradorFinal/impl/OdontologoDaoMemoria.java
+++ integradorFinal/src/main/java/com/backen/integradorFinal/impl/OdontologoDaoMemoria.java
@@ -10,7 +10,7 @@ import java.util.logging.Logger;
 
 
 public class OdontologoDaoMemoria implements Dao<Odontologo> {
-    private static final Logger LOGGER = Logger.getLogger(OdontologoDaoMemoria.class);
+    private static final Logger LOGGER = Logger.getLogger(String.valueOf(OdontologoDaoMemoria.class));
     private List<Odontologo> odontologoRepositorio;
 
     public OdontologoDaoMemoria() {
@@ -29,5 +29,20 @@ public class OdontologoDaoMemoria implements Dao<Odontologo> {
     public List<Odontologo> listar() {
         LOGGER.info("Listado de todos los odontologos: \n" + String.valueOf(this.odontologoRepositorio));
         return this.odontologoRepositorio;
+    }
+
+    @Override
+    public void eliminar(int id) {
+
+    }
+
+    @Override
+    public Odontologo buscarPorId(int id) {
+        return null;
+    }
+
+    @Override
+    public Odontologo buscarPorCriterio(String criterio) {
+        return null;
     }
 }
--- integradorFinal/src/main/java/com/backen/integradorFinal/service/OdontologoService.java
+++ integradorFinal/src/main/java/com/backen/integradorFinal/service/OdontologoService.java
@@ -1,6 +1,5 @@
 package com.backen.integradorFinal.service;
 
-
 import com.backen.integradorFinal.impl.OdontologoDaoH2;
 import com.backen.integradorFinal.model.Odontologo;
 
@@ -10,18 +9,18 @@ public class OdontologoService {
 
     private OdontologoDaoH2 odontologoDaoH2;
 
-
     public OdontologoService() {
         this.odontologoDaoH2 = new OdontologoDaoH2();
     }
 
     public Odontologo registrarOdontologo(Odontologo odontologo) {
+        // Código para registrar el odontólogo
+        return odontologo;
+    }
 
+    public List<Odontologo> listarOdontologos() {
+        List<Odontologo> listaOdontologos = odontologoDaoH2.listar();
 
-        public List<Odontologo> listarOdontologos () {
-            List<Odontologo> listaOdontologos = odontologoDaoH2.listar();
-
-            return listaOdontologos;
-        }
+        return listaOdontologos;
     }
 }
